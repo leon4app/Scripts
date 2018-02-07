45377921b1759be87db03644f22307005671570f
Leon <Leon4w@outlook.com>
2018-02-07 14:35:22 +0800

    Initial commit

---
+++ RedirectSpecs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "This script is used to redirect source of podspec files to our custom server."
+if [ $# != 1 ]
+then
+	echo "Usage: sh RedirectSpecs.sh dir"
+	echo "@param dir Target directory you want to apply this script."
+else
+	echo "Applyint script to $1"
+	export LC_COLLATE='C'
+	export LC_CTYPE='C'
+	sed -i '' 's|http.*\/\(.*\)\.git|http:\/\/git.dev.cmrh.com\/FrontEnd\/Architecture\/iOS\/\1.git|g' `grep 'http.*\/\(.*\)\.git' -rl ./`
+	echo "Done!"
+fi
